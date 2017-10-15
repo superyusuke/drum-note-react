46eda3466ac737cc2a836f3bdb39794a26b104bd
NAKANISHI Yusuke <nakanishiyusuke0000@gmail.com>
2017-10-15 22:11:16 +0900

    apply standard

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import reducer from './reducers';
-import Cell from './containers/Cell';
+import React from 'react'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './reducers'
+import Cell from './containers/Cell'
 
-let store = createStore(reducer);
+let store = createStore(reducer)
 
 const App = () => (
-    <Provider store={store}>
-        <div>
-            <Cell/>
-        </div>
-    </Provider>
-);
+  <Provider store={store}>
+    <div>
+      <Cell />
+    </div>
+  </Provider>
+)
 
 store.subscribe(() =>
     console.log(store.getState())
-);
+)
 
-export default App;
+export default App
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,22 @@
 const text = (state = {upper: 'upper', lower: 'lower'}, action) => {
-    switch (action.type) {
-        case 'CHANGE_TEXT':
-            switch (action.position) {
-                case 'upper':
-                    return (
+  switch (action.type) {
+    case 'CHANGE_TEXT':
+      switch (action.position) {
+        case 'upper':
+          return (
                         Object.assign({}, state, {upper: action.text})
-                    );
+          )
 
-                case 'lower':
-                    return (
+        case 'lower':
+          return (
                         Object.assign({}, state, {lower: action.text})
-                    );
-                default:
-                    return state;
-            }
+          )
         default:
-            return state;
-    }
-};
+          return state
+      }
+    default:
+      return state
+  }
+}
 
-export default text;
-
+export default text
